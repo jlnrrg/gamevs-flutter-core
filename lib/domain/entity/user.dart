c35407241a16d29a6e6a4e3c9d171b3739d7ce46
jlnrrg <julian.roerig.k@gmail.com>
2024-12-27 17:34:09 +0100

    User: add datetime fields

--- lib/domain/entity/user.dart
+++ lib/domain/entity/user.dart
@@ -1,11 +1,19 @@
 import 'package:uuid/uuid.dart';
 
 class User {
-  const User(
-      {required this.id, this.firstName, this.lastName, required this.email});
+  const User({
+    required this.id,
+    this.firstName,
+    this.lastName,
+    required this.email,
+    required this.createdAt,
+    required this.updatedAt,
+  });
 
   final UuidValue id;
   final String? firstName;
   final String? lastName;
   final String email;
+  final DateTime createdAt;
+  final DateTime updatedAt;
 }
