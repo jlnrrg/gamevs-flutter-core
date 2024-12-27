f21eecc5835bea82d90abf4c6b4e4cc9a929ffb0
jlnrrg <julian.roerig.k@gmail.com>
2024-12-27 12:20:27 +0100

    User: add uuid

--- lib/domain/entity/user.dart
+++ lib/domain/entity/user.dart
@@ -1,6 +1,10 @@
-class User {
-  const User({this.firstName, this.lastName, required this.email});
+import 'package:uuid/uuid.dart';
 
+class User {
+  const User(
+      {required this.id, this.firstName, this.lastName, required this.email});
+
+  final UuidValue id;
   final String? firstName;
   final String? lastName;
   final String email;
