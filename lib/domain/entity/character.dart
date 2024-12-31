5a79a279a730f2c6b5d83a5c4cd177cf4d9c9bba
jlnrrg <julian.roerig.k@gmail.com>
2024-12-31 01:42:35 +0100

    Fight: add FightKill class and ids

--- lib/domain/entity/character.dart
+++ lib/domain/entity/character.dart
@@ -1,15 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:gamevs_core/domain/entity/misc.dart';
 import 'package:gamevs_core/domain/entity/user.dart';
+import 'package:uuid/uuid.dart';
 
-class PlayedCharacter {
-  const PlayedCharacter({
+class FightPlayer {
+  const FightPlayer({
+    required this.id,
     required this.user,
     this.character = const [],
+    required this.createdAt,
+    required this.updatedAt,
   });
 
+  final UuidValue id;
   final User user;
   final List<Character> character;
+  final DateTime createdAt;
+  final DateTime updatedAt;
 }
 
 abstract class Character implements FlexibleEnum {
--- lib/domain/entity/fight.dart
+++ lib/domain/entity/fight.dart
@@ -2,47 +2,69 @@ import 'package:gamevs_core/domain/entity/character.dart';
 import 'package:gamevs_core/domain/entity/game.dart';
 import 'package:gamevs_core/domain/entity/settings.dart';
 import 'package:gamevs_core/domain/entity/stage.dart';
+import 'package:gamevs_core/domain/entity/user.dart';
+import 'package:uuid/uuid.dart';
 
 class Fight {
   const Fight({
     required this.id,
     required this.game,
     this.players = const [],
-    this.stage = const [],
+    this.stages = const [],
     this.result,
-    required this.settings,
+    this.settings,
     required this.createdAt,
     required this.updatedAt,
   });
 
-  final String id;
+  final UuidValue id;
   final Game game;
-  final List<PlayedCharacter> players;
-  final List<Stage> stage;
+  final List<FightPlayer> players;
+  final List<Stage> stages;
   final List<FightResult>? result;
-  final Settings settings;
+  final Settings? settings;
   final DateTime createdAt;
   final DateTime updatedAt;
 }
 
 class FightResult {
   const FightResult({
+    required this.id,
     required this.fight,
     required this.player,
     this.rank,
     this.time,
     this.points,
-    this.kill,
-    this.death,
+    this.kills,
+    required this.createdAt,
+    required this.updatedAt,
   });
 
+  final UuidValue id;
   final Fight fight;
-  final PlayedCharacter player;
+  final FightPlayer player;
 
   // result
   final int? rank;
   final Duration? time;
   final int? points;
-  final List<PlayedCharacter>? kill;
-  final List<PlayedCharacter>? death;
+  final List<FightKill>? kills;
+  final DateTime createdAt;
+  final DateTime updatedAt;
+}
+
+class FightKill {
+  const FightKill({
+    required this.id,
+    this.killer,
+    required this.victim,
+    required this.createdAt,
+    required this.updatedAt,
+  });
+
+  final UuidValue id;
+  final User? killer;
+  final User victim;
+  final DateTime createdAt;
+  final DateTime updatedAt;
 }
