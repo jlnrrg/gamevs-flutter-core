2caa10e13e97b4634342084f078804b84f623114
jlnrrg <julian.roerig.k@gmail.com>
2025-01-20 18:13:02 +0100

    Failure: add easy mapping

--- lib/domain/failure/api_failure.dart
+++ lib/domain/failure/api_failure.dart
@@ -4,7 +4,8 @@ part 'api_failure.mapper.dart';
 
 @MappableClass()
 sealed class ApiFailure with ApiFailureMappable {
-  static ApiFailure fromError(Object, StackTrace) {
+  static ApiFailure fromError(Object o, StackTrace) {
+    if (o is ApiFailure) return o;
     // TODO(jln): add mapping
     throw UnimplementedError();
   }
